f8a4d3069cea0605f64925ec85767c7faca96f6b
isabellamaltoni <amaltoniplaza@itba.edu.ar>
2023-04-04 18:00:54 -0300

    ingreso datos

--- clase_usuario.py
+++ clase_usuario.py
@@ -1,6 +1,12 @@
 class Usuario():
     def __init__(self,nombre,fec_nac,mail):
         self.nombre=nombre 
+        self.fec_nac=fec_nac
+        self.mail=mail
+
+    def ingreso_datos():
 
 
-#aca hice cl
+        
+
+
